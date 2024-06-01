3a23585763848e7a514287fda1a33666e72aa58a
Andrey Golikov <aregularmailaddress@gmail.com>
2024-06-01 21:38:02 +0600

    Change main.py to use new modules
    Load data provider from config, remove test route, add some endpoint drafts

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI, Request, HTTPException
+from typing import Any
+from general import get_config
+from fastapi import FastAPI, Request, HTTPException, Response
 from fastapi.responses import HTMLResponse
-
+import db
+import json
+from config import config
 import users.main as users
-from page_constructor.main import TestPageConstructor
 
+
+data_provider: db.DataProvider = vars(db)[config.get("dataProvider")]()
 
 app = FastAPI()
 
@@ -12,14 +17,31 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/test/", response_class=HTMLResponse)
-async def test_page():
-    return TestPageConstructor.construct()
-
 @app.get("/api/user/")
 async def get_user(request: Request):
     id = request.cookies.get("sessionKey")
     response = users.get_user(id)
     if not response:
-        raise HTTPException(status_code=401, detail="User is not authentificated")
+        raise HTTPException(status_code=401, detail="User is unauthenticated")
     return response
+
+@app.post("/api/user/login")
+async def login(username: str, password: str, response: Response):
+    #temp
+    response.set_cookie("sessionKey", "123")
+    return {"message": "Login successful"}
+
+# student sends new lab
+@app.put("/api/card/create")
+async def create_card():
+    raise NotImplementedError
+
+# teacher gets labs list
+
+# teacher gets preview numbers 
+
+# teacher gets a lab
+
+# teacher accepts, denies and leaves a comment to a lab request, otlozhenyye
+
+# get labs tasks list
