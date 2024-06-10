859b1538189f5214e244e172acf48c3de96b2006
Nadi13 <nadia_yudintseva@mail.ru>
2024-06-10 15:28:06 +0600

    fix; config

--- src/components/Screen1/Main/RequestList/RequestList.tsx
+++ src/components/Screen1/Main/RequestList/RequestList.tsx
@@ -7,30 +7,38 @@ import axios from 'axios';
 
 const RequestList = () => {
     const [requests, setRequests] = useState<Array<{
-        name: string;
-        number: string;
-        work: string;
-        course: string;
-        date: string;
-        deadline: boolean;
+        id: Int16Array,
+        studenid: string;
+        labid: string;
+        content: string;
+        comments: string;
+        lecturerid: string;
+        variant: Int16Array;
+        info: {};
+        status: string;
+        creationdate: Date;
     }>>([]);
     const navigate = useNavigate();
 
     useEffect(() => {
         const fetchRequests = async () => {
           const { data } = await axios.get(
-            "http://127.0.0.1:8000/api/user?sessionKey=123"
+            "http://127.0.0.1:8000/api/cards?sessionKey=123"
           );
           const requests = Object.values(data) as any[];
           console.log(requests)
 
           setRequests(requests.map((request) => ({
-            name: request.name,
-            number: request.number,
-            work: request.work,
-            course: request.course,
-            date: request.date,
-            deadline: request.deadline
+            id: request.id,
+            studenid: request.studentid,
+            labid: request.labid,
+            content: request.content,
+            comments: request.comments,
+            lecturerid: request.lecturerid,
+            variant: request.variant,
+            info: request.info,
+            status: request.status,
+            creationdate: request.creationdate
           })));
         };
 
